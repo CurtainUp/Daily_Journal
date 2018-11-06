8968ae3687e01dfdd207056a63a30192ff106737
Elyse Dawson <dawson.elyse@gmail.com>
2018-11-06 15:24:21 -0600

    Entries are now created dynamically and appended to dom

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -6,16 +6,10 @@
 API.getJournalEntries()
     .then(entries => {
         entries.forEach((entry) => {
-            const htmlEntry = htmlJournalEntries.createJournalComponent(entry)
-            insertEntry.renderEntry(htmlEntry)
+            const singleEntry = journalEntries.createJournalComponent(entry)
+            insertEntry.renderEntry(singleEntry)
         })
     })
-
-// In your main JavaScript module (journal.js) add a click event listener to
-// the Record Journal Entry button at the bottom of your form. When the user
-// clicks the button, you need to create a new entry in your API.
-// The HTTP method that you use to create resources is POST. Guidance on
-// syntax is provided below.
 
 // Empty entry object (this needs to eventually be a class. This is absurd)
 const entry = {
